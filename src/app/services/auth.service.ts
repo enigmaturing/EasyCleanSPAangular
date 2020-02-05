1ec7f2139a150e40eef384d11b8b62d796bae5de
Javier Gonzalez <javier.glez.martin@gmail.com>
2020-02-05 18:29:18 +0100

    FIX: Make login method RETURN a observable

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -12,7 +12,7 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   login(model: any) {
-    this.http.post(this.baseUrl + 'login', model).pipe(
+    return this.http.post(this.baseUrl + 'login', model).pipe(
       map((response: any) => {
         const user = response;
         if (user) {
