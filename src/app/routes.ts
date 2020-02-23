a91901ccef4f572e34292389d28f4ff6f2f28edf
Javier Gonzalez <javier.glez.martin@gmail.com>
2020-02-23 19:11:40 +0000

    Set route home as an empty string
    [v. 67]
    - This avoids the problem caused when the user looks for the route localhost:4200  (with no /home)

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -7,10 +7,10 @@ import { SalesListComponent } from './sales-list/sales-list.component';
 import { AuthGuard } from './guards/auth.guard';
 
 export const appRoutes: Routes = [
+    { path: '', component: HomeComponent},
     { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]},
-    { path: 'home', component: HomeComponent},
     { path: 'sales', component: SalesListComponent, canActivate: [AuthGuard]},
     { path: 'clients', component: ClientsListComponent, canActivate: [AuthGuard]},
     { path: 'machines', component: MachinesListComponent, canActivate: [AuthGuard]},
-    { path: '**', redirectTo: 'home', pathMatch: 'full'},
+    { path: '**', redirectTo: '', pathMatch: 'full'},
 ];
