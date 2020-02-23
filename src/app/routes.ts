d192ecb7ce12d49b95bfb44ee02b36a1f1d09620
Javier Gonzalez <javier.glez.martin@gmail.com>
2020-02-23 17:05:14 +0000

    Protect routes with a guard
    [v. 66 Protect routes with a route guard]
    - Generate a guard called auth with: ng g guard guards/auth
    - Make the guard implement the interface CanActivate
    - In the routes.ts file, protect the desired routes with that guard

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -4,12 +4,13 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { ClientsListComponent } from './clients-list/clients-list.component';
 import { MachinesListComponent } from './machines-list/machines-list.component';
 import { SalesListComponent } from './sales-list/sales-list.component';
+import { AuthGuard } from './guards/auth.guard';
 
 export const appRoutes: Routes = [
-    { path: 'dashboard', component: DashboardComponent},
+    { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]},
     { path: 'home', component: HomeComponent},
-    { path: 'sales', component: SalesListComponent},
-    { path: 'clients', component: ClientsListComponent},
-    { path: 'machines', component: MachinesListComponent},
+    { path: 'sales', component: SalesListComponent, canActivate: [AuthGuard]},
+    { path: 'clients', component: ClientsListComponent, canActivate: [AuthGuard]},
+    { path: 'machines', component: MachinesListComponent, canActivate: [AuthGuard]},
     { path: '**', redirectTo: 'home', pathMatch: 'full'},
 ];
