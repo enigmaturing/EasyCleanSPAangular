5d8764045f063d9758b2b9fc0e8458627e7cb07e
Javier Gonzalez <javier.glez.martin@gmail.com>
2020-03-07 11:51:13 +0000

    Store our apiUrl in enviroment.ts
    [v. 84 Create another angular service]
    - apiUrl in development differs crom apiUrl in production mode. So, we can control this storing our apiUrl in the enviroment.ts file (by the moment we dont have a production apiUrl so we dont need to store anyting inside of eviroment.prod.ts)
    - We could also avoid commiting enviroment.prod.ts file so that our apiUrl is not shared in the remote repo.

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../models/user';
+import { environment } from '../../environments/environment';
 
 const httpOptions = {
   headers: new HttpHeaders({
@@ -14,15 +15,15 @@ const httpOptions = {
 })
 export class UserService {
 
-  baseUrl = 'http://localhost:5000/api/';
+  baseUrl = environment.apiUrl + 'users/';
 
   constructor(private http: HttpClient) { }
 
   getUsers(): Observable<User[]>{
-    return this.http.get<User[]>(this.baseUrl + 'users', httpOptions);
+    return this.http.get<User[]>(this.baseUrl, httpOptions);
   }
 
   getUser(id): Observable<User>{
-    return this.http.get<User>(this.baseUrl + 'users/' + id, httpOptions);
+    return this.http.get<User>(this.baseUrl + id, httpOptions);
   }
 }
