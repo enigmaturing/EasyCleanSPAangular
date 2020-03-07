6623e90cdb73255f19d75ead9a0eab22f0eff6bc
Javier Gonzalez <javier.glez.martin@gmail.com>
2020-03-07 20:01:43 +0000

    Show client-card as child component of client-list
    [v. 86 - Create client cards to display on clients-list page]
    - Parent to child component communication using @Input properties

--- src/app/clients/client-card/client-card.component.ts
+++ src/app/clients/client-card/client-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-client-card',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./client-card.component.css']
 })
 export class ClientCardComponent implements OnInit {
+
+  @Input() user: User;
 
   constructor() { }
 
