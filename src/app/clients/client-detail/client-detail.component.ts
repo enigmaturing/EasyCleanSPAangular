2d78c4cfc20fa71f3a8250ffe67261fa7b2307c9
Javier Gonzalez <javier.glez.martin@gmail.com>
2020-03-09 10:06:06 +0000

    Set the route to client-details depending on id
    [v. 90 Create the member detailed view component class]
    - Depending on the client-card clicked, display the client-detailed component refering to the clikced client

--- src/app/clients/client-detail/client-detail.component.ts
+++ src/app/clients/client-detail/client-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { AlertifyService } from '../../services/alertify.service';
+import { ActivatedRoute } from '@angular/router';
+import { UserDetailed } from 'src/app/models/user-detailed';
 
 @Component({
   selector: 'app-client-detail',
@@ -7,9 +11,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientDetailComponent implements OnInit {
 
-  constructor() { }
+  user: UserDetailed;
+
+  constructor(private userService: UserService,
+              private alertify: AlertifyService,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.loadUser();
+  }
+
+  loadUser() {
+    this.userService.getUser(this.route.snapshot.params.id).subscribe((user: UserDetailed) => {
+      this.user = user;
+    }, error => {
+      this.alertify.error(error);
+    });
   }
 
 }
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -5,12 +5,14 @@ import { ClientsListComponent } from './clients/clients-list/clients-list.compon
 import { MachinesListComponent } from './machines-list/machines-list.component';
 import { SalesListComponent } from './sales-list/sales-list.component';
 import { AuthGuard } from './guards/auth.guard';
+import { ClientDetailComponent } from './clients/client-detail/client-detail.component';
 
 export const appRoutes: Routes = [
     { path: '', component: HomeComponent},
     { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]},
     { path: 'sales', component: SalesListComponent, canActivate: [AuthGuard]},
     { path: 'clients', component: ClientsListComponent, canActivate: [AuthGuard]},
+    { path: 'clients/:id', component: ClientDetailComponent, canActivate: [AuthGuard]},
     { path: 'machines', component: MachinesListComponent, canActivate: [AuthGuard]},
     { path: '**', redirectTo: '', pathMatch: 'full'},
 ];
