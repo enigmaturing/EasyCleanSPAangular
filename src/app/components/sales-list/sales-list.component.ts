d3769dc0d54ee14f5f622c1db8d9426324df9938
Javier Gonzalez <javier.glez.martin@gmail.com>
2020-03-19 16:18:42 +0000

    Show machineUsags in Sales section

--- src/app/components/sales-list/sales-list.component.ts
+++ src/app/components/sales-list/sales-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { SalesService } from 'src/app/services/sales.service';
+import { AlertifyService } from 'src/app/services/alertify.service';
+import { MachineUsage } from 'src/app/models/machine-usage';
 
 @Component({
   selector: 'app-sales-list',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SalesListComponent implements OnInit {
 
-  constructor() { }
+  machineUsages: MachineUsage[];
+
+  constructor(private salesService: SalesService,
+              private alertify: AlertifyService) { }
+
+  loadMachineUsages() {
+    this.salesService.getMachineUsages().subscribe((machineUsages: MachineUsage[]) => {
+      this.machineUsages = machineUsages;
+      console.log(this.machineUsages);
+    }, error => {
+      this.alertify.error(error);
+    });
+  }
 
   ngOnInit() {
+    this.loadMachineUsages();
   }
+
 
 }
---
+++ src/app/services/sales.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { MachineUsage } from '../models/machine-usage';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SalesService {
+
+  baseUrl = environment.apiUrl + 'sales/';
+
+  constructor(private http: HttpClient) { }
+
+  getMachineUsages(): Observable<MachineUsage[]> {
+    return this.http.get<MachineUsage[]>(this.baseUrl);
+  }
+}
