428214f4338454f01615a65e9311d184a8acef14
Javier Gonzalez <javier.glez.martin@gmail.com>
2020-03-21 16:46:27 +0000

    Change durationInMinutes to number

--- src/app/models/tariff.ts
+++ src/app/models/tariff.ts
@@ -2,7 +2,7 @@ export interface Tariff {
     id: number;
     name: string;
     price: number;
-    durationInMinutes: string;
+    durationInMinutes: number;
     isActive: boolean;
     machineId: number;
 }
